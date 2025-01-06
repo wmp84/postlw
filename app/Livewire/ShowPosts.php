714b95fb6522638b507c981014f2f6e035c65f07
D-WILD\Wild <wmp84@hotmail.com>
2025-01-05 19:50:50 -0500

    Posts Migración Componente livewire Factory

---
+++ app/Livewire/ShowPosts.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Post;
+use Livewire\Component;
+
+class ShowPosts extends Component
+{
+    public function render()
+    {
+        $posts= Post::all();
+        return view('livewire.show-posts', compact('posts'));
+    }
+}
---
+++ resources/views/livewire/show-posts.blade.php
@@ -0,0 +1,8 @@
+<div>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+    {{$posts}}
+</div>
